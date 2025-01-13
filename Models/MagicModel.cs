afeabe3f6a6decfe7d53d1ed71d0be9a325b2f9e
ChristmasLover12345 <madnes0205@gmail.com>
2025-01-13 13:40:09 -0800

    basics

---
+++ Models/MagicModel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HerediaJ_C_2_All4OneAPI.Models
+{
+    public class MagicModel
+    {
+        public int Id {get; set;}
+        public string? Content {get; set;}
+    }
+}
